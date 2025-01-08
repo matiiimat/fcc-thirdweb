f0d12f3def1f0f888bd9cadbc158a6cb5b5c7b53
matiiimat <109181275+matiiimat@users.noreply.github.com>
2025-01-08 22:08:50 +0100

    Update page.tsx

--- src/app/shootingTraining/page.tsx
+++ src/app/shootingTraining/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
       <h1>Penalty Shooting Game</h1>
-      <p>Swipe to shoot the ball into the goal. You have 5 shots!</p>
+      <p>Swipe to shoot the ball into the goal. You have 5 shots</p>
       <ShootingGame />
     </div>
   );
